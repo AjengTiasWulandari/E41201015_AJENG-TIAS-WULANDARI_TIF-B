191a5be66b557844a8b239440cc1d9597c182871
AjengTiasWulandari <ajengtiaswulandari@gmail.com>
2021-03-23 10:35:25 +0700

    Create Tugas1.java

---
+++ MINGGU-3/Tugas1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @ajengtiaswulandari
+ */
+public class Tugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama[] = {"Ajeng","Tias","Wulandari"};
+        
+        for(int j=0; j<nama.length; j++){
+            System.out.print(nama[j]);
+            System.out.print(" ");
+        }
+    }
+    
+}
