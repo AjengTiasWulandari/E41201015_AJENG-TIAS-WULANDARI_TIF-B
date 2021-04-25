5aad6a8bfda4a5f05830a5879cb76ba2c03f797d
AjengTiasWulandari <ajengtiaswulandari@gmail.com>
2021-04-25 20:20:58 +0700

    Create C.java

---
+++ MINGGU-7/TugasB/C.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasB;
+
+/**
+ *
+ * @ajengtiawulandari
+ */
+public class C extends A{
+    public void callthis(){
+        System.out.println("Inside Class C's Method!");
+    }
+}
