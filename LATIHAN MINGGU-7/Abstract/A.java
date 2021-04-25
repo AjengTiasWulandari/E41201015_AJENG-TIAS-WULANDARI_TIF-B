226a75f27432cc394344511fa11aa5040ee478bb
AjengTiasWulandari <ajengtiaswulandari@gmail.com>
2021-04-25 20:14:43 +0700

    Create A.java

---
+++ LATIHAN MINGGU-7/Abstract/A.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abstract;
+
+/**
+ *
+ * @ajengtiaswulandari
+ */
+public abstract class A { //Deklarasi Class
+    abstract public void method2();
+    // method konkrit yang memiliki implementasi
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+    
+}
