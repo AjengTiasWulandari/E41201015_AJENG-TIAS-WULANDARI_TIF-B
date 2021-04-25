35f4ac9ca286941d1af4d658340b3bf83e6841a6
AjengTiasWulandari <ajengtiaswulandari@gmail.com>
2021-04-25 20:16:28 +0700

    Create A.java

---
+++ LATIHAN MINGGU-7/Konstruktor/A.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Konstruktor;
+
+/**
+ *
+ * @ajengtiaswulandari
+ */
+public class A {
+    A(){} // Konstruktor default, diperlukan hanya agar program dapat
+          //dikompilasi
+    A (String param1){
+        System.out.println("Konstruktor class A dengan parameter " +param1 + "dieksekusi ...");
+    }
+    
+}
